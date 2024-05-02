1fa6035a608b88fa09588ab1d29730daa1d3c989
Aidan Portwood <aidanportwood0@gmail.com>
2024-05-02 13:59:00 -0700

    add code to pass test

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,20 @@ function formatInput(input) {
   };
 return numberArray
 };
+
+function replaceNumber(numberArray) {
+  let mixedArray = [];
+  numberArray.forEach(function(number) {
+      console.log(number)
+    if (number.toString().includes(3) === true) {
+      mixedArray.push("Won't you be my neighbor?");
+    } else if (number.toString().includes(2) === true) {
+      mixedArray.push("Boop");
+    } else if (number.toString().includes(1) === true) {
+      mixedArray.push("Beep!");
+    } else {
+      mixedArray.push(number);
+    };
+  });
+return mixedArray
+};
